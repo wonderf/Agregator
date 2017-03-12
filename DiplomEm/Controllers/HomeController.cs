38d4f52d020ebfc74f5527977ddfd03198a16898
Stas <pobolinstas@gmail.com>
2017-03-12 16:00:24 +0500

    add request url

--- DiplomEm/Controllers/HomeController.cs
+++ DiplomEm/Controllers/HomeController.cs
@@ -1,24 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using EdgeJs;
+using System;
+using System.Net;
 using System.Web.Mvc;
+
 
 namespace DiplomEm.Controllers
 {
     public class HomeController : Controller
     {
-        //partial view?
-        // GET: Home
+
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public ActionResult Index()
         {
             return View();
         }
-        //return view with page??
-        [HttpPost]
-        public ActionResult Update()
+
+        public PartialViewResult GetUrlPage(string url)
         {
-            return View();
+            String answer = String.Empty;
+            if (!string.IsNullOrEmpty(url))
+            {
+                try {
+                    WebClient wc = new WebClient();
+                    wc.Encoding = System.Text.Encoding.UTF8;
+                    answer = wc.DownloadString(url);
+                }
+                catch(Exception e)
+                {
+                    logger.Error(e);
+                }
+            }
+            ViewData["url"] = answer;
+            return PartialView();
         }
+        public ActionResult GetJsonResult(string code)
+        {
+            String answer = String.Empty;
+            if (!string.IsNullOrEmpty(code))
+            {
+                try
+                {
+                    var func = Edge.Func(@"
+                            return function (data, callback) {
+                                callback(null, 'Node.js welcomes ' + data);
+                             }");
+                }
+                catch (Exception e)
+                {
+                    logger.Error(e);
+                }
+            }
+            ViewData["json"] = answer;
+            return PartialView();
+        }
+        
     }
 }
